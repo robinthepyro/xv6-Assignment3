9730e22f81316e546bd9cc2b7de43ce64b608df4
robinthepyro <robinblue072@gmail.com>
2024-10-15 15:10:36 +1300

    Re-Init + Syscall Was using wrong version of xv6 apparently :(

---
+++ kernel/semaphore.h
@@ -0,0 +1,23 @@
+struct semaphore {
+  int value;
+  struct spinlock lk;
+};
+
+// Prototype of the three semaphore functions in semaphore.c
+void initsema(struct semaphore*, int);
+int downsema(struct semaphore*);
+int upsema(struct semaphore*);
+
+
+// Read/Write Semaphore
+struct rwsemaphore {
+  // TODO add contents
+  // likely turnstile + reader count + mutex
+};
+
+void initrwsema(struct rwsemaphore *);
+int downreadsema(struct rwsemaphore *);
+int upreadsema(struct rwsemaphore *);
+void downwritesema(struct rwsemaphore *);
+void upwritesema(struct rwsemaphore *);
+
